209797a1ef76cbac562a64f76ab1b4842b4bb8fe
Ban-develop <topnios@gmail.com>
2021-05-17 22:26:11 +0900

    version 0.0.2

---
+++ config/Dev.js
@@ -0,0 +1,4 @@
+const mongoURI =
+  "mongodb+srv://topnios:skek0402@cluster0.eopcl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+
+module.exports = { mongoURI };
--- index.js
+++ index.js
@@ -2,22 +2,36 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const { User } = require("./modules/User");
+const { mongoURI } = require("./config/Dev");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
 
 mongoose
-  .connect(
-    "mongodb+srv://topnios:skek0402@cluster0.eopcl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true,
-      useFindAndModify: false,
-    }
-  )
-  .then(() => console.log("Connectd...."))
-  .catch((err) => console.log("Err..."));
+  .connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() => console.log(`Connectd....`))
+  .catch((err) => console.log(`Err..${err}`));
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.send("Hello World!!!!!");
+});
+
+app.post("/register", (req, res) => {
+  const user = new User(req.body);
+  user.save((err, doc) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
 });
 
 app.listen(port, () => {
